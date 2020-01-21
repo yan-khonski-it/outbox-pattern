d6c5acedbb7c81f3d13ae69f0e2026c43d732395
y.khonski <y.khonski@kendaxa.com>
2020-01-21 13:04:58 +0100

    Minor formatting

---
+++ link-debezium-with-kafka.sh
@@ -0,0 +1,20 @@
+curl -X POST \
+  http://localhost:8083/connectors/ \
+  -H 'content-type: application/json' \
+  -d '{
+   "name": "student-outbox-connector",
+   "config": {
+      "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
+      "tasks.max": "1",
+      "database.hostname": "postgres",
+      "database.port": "5432",
+      "database.user": "user",
+      "database.password": "password",
+      "database.dbname": "studentdb",
+      "database.server.name": "pg-outbox-server",
+      "tombstones.on.delete": "false",
+      "table.whitelist": "public.outbox",
+      "transforms": "outbox",
+      "transforms.outbox.type": "com.sohan.transform.CustomTransformation"
+   }
+}'
